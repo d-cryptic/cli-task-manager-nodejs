4be51318e9b68373a89c4e906cee407befa07605
Barun Debnath <barundebnath91@gmail.com>
2021-11-30 01:11:36 +0530

    help commands done

--- task.js
+++ task.js
@@ -1,5 +1,28 @@
-const chalk = require("chalk");
 const yargs = require("yargs");
 
 // Customize yargs version
 yargs.version("1.1.0");
+
+// load fs
+const fs = require("fs");
+// read the file
+const content = fs.readFileSync("./helpMessage.txt");
+// print it
+// console.log(content.toString());
+
+// Create help command
+if (process.argv.slice(2) == false) {
+  console.log(content.toString());
+}
+
+yargs.help(false).command({
+  command: "help",
+  describe: "Show all commands",
+  handler() {
+    // console.log(content.toString());
+  },
+});
+
+yargs.parse();
+
+// console.log(process.argv.slice(2) == false);
