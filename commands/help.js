72c2c549bdea89759e6c7305d1b40bf09bc20ca7
Barun Debnath <barundebnath91@gmail.com>
2021-11-30 02:40:36 +0530

    help commands done final

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,24 @@
-Usage :-
+// load fs
+const fs = require("fs");
+
+// read the file
+const content = `Usage :-
 $ ./task add 2 hello world     # Add a new item with priority 2 and text "hello world" to the list
 $ ./task ls                    # Show incomplete priority list items sorted by priority in ascending order
 $ ./task del NUMBER   # Delete the incomplete item with the given priority number
 $ ./task done NUMBER  # Mark the incomplete item with the given PRIORITY_NUMBER as complete
 $ ./task help                  # Show usage
-$ ./task report                # Statistics
+$ ./task report                # Statistics`;
+
+argv = process.argv.slice(2);
+
+// Create help command
+
+const helpCommand = () => {
+  // When running only "./task"
+  if (process.argv.slice(2) == false || argv[0] === "help") {
+    console.log(content.toString());
+  }
+};
+
+module.exports = helpCommand;
--- task.js
+++ task.js
@@ -1,45 +1,67 @@
-// load fs
 const fs = require("fs");
-// read the file
-const content = fs.readFileSync("./helpMessage.txt");
-// print it
-// console.log(content.toString());
 
 argv = process.argv.slice(2);
 
-// Create help command
-if (process.argv.slice(2) == false) {
-  console.log(content.toString());
-}
+// Help command
+const help = require("./commands/help.js");
+help();
 
-if (argv[0] === "help") {
-  console.log(content.toString());
-}
+// if (argv[0] === "add") {
+//   if (argv[1] === undefined || argv[2] === undefined) {
+//     console.log("Error: Missing tasks string. Nothing added!");
+//   } else {
+//     console.log(`Added task: "${argv[2]}" with priority ${argv[1]}`);
+//   }
 
-if (argv[0] === "add") {
-  if (argv[1] === undefined || argv[2] === undefined) {
-    console.log("Error: Missing tasks string. Nothing added!");
-  } else {
-    console.log(`Added task: "${argv[2]}" with priority ${argv[1]}`);
-  }
+//   // write to a new file named 2pac.txt
+//   fs.writeFile("./task.txt", `${argv[1]} ${argv[2]}`, (err) => {
+//     // throws an error, you could also catch it here
+//     if (err) throw err;
 
-  // write to a new file named 2pac.txt
-  fs.writeFile("./task.txt", `${argv[1]} ${argv[2]}`, (err) => {
-    // throws an error, you could also catch it here
-    if (err) throw err;
+//     // success case, the file was saved
+//     console.log("saved!");
+//   });
+// }
 
-    // success case, the file was saved
-    console.log("Lyric saved!");
-  });
-}
+// // console.log(process.argv.slice(2));
 
-const fs = require("fs");
+// let listFile = {};
 
-fs.readFile("file.txt", "utf-8", (err, data) => {
-  if (err) {
-    throw err;
-  }
-  console.log(data);
-});
+// fs.readFile("task.txt", "utf-8", (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
 
-// console.log(process.argv.slice(2));
+//   data1 = data.split("\n");
+//   console.log(data1);
+// });
+
+// // Reading, appending and sorting file
+
+// const readFile = () => {
+//   fs.readFile("task.txt", "utf-8", (err, data) => {
+//     if (err) {
+//       throw err;
+//     }
+
+//     data1 = data.split("\n");
+// 	  let data2 = {};
+//     let length = data1.length;
+//     for (i = 0; i < length; i++) {
+//       data2 = data1[i].split(" ");
+// 		listFile[ i ] = { data2[ 0 ]: data2[ 1 ] };
+//     }
+//   });
+// };
+
+// const appendFile = (data) => {
+//   fs.appendFile("task.txt", data, function (err) {
+//     if (err) {
+//       throw err;
+//     } else {
+//       console.log("Saved!");
+//     }
+//   });
+// };
+
+// const sortData = (data) => {};
