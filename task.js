899832335dcc6b0a7642cd446e6a7164227ce14f
Barun Debnath <barundebnath91@gmail.com>
2021-11-30 01:46:23 +0530

    add command done and task saving to txt file

--- task.js
+++ task.js
@@ -1,8 +1,3 @@
-const yargs = require("yargs");
-
-// Customize yargs version
-yargs.version("1.1.0");
-
 // load fs
 const fs = require("fs");
 // read the file
@@ -10,19 +5,41 @@ const content = fs.readFileSync("./helpMessage.txt");
 // print it
 // console.log(content.toString());
 
+argv = process.argv.slice(2);
+
 // Create help command
 if (process.argv.slice(2) == false) {
   console.log(content.toString());
 }
 
-yargs.help(false).command({
-  command: "help",
-  describe: "Show all commands",
-  handler() {
-    // console.log(content.toString());
-  },
+if (argv[0] === "help") {
+  console.log(content.toString());
+}
+
+if (argv[0] === "add") {
+  if (argv[1] === undefined || argv[2] === undefined) {
+    console.log("Error: Missing tasks string. Nothing added!");
+  } else {
+    console.log(`Added task: "${argv[2]}" with priority ${argv[1]}`);
+  }
+
+  // write to a new file named 2pac.txt
+  fs.writeFile("./task.txt", `${argv[1]} ${argv[2]}`, (err) => {
+    // throws an error, you could also catch it here
+    if (err) throw err;
+
+    // success case, the file was saved
+    console.log("Lyric saved!");
+  });
+}
+
+const fs = require("fs");
+
+fs.readFile("file.txt", "utf-8", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log(data);
 });
 
-yargs.parse();
-
-// console.log(process.argv.slice(2) == false);
+// console.log(process.argv.slice(2));
