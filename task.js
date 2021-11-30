f7fabb338cfd790c05b9b148d505f18bebd20de3
Barun Debnath <barundebnath91@gmail.com>
2021-11-30 12:12:47 +0530

    help commands done final

--- task.js
+++ task.js
@@ -1,34 +1,32 @@
-const fs = require("fs");
 const help = require("./commands/help.js");
-const add = require("./commands/add.js");
-const list = require("./commands/list.js");
-const done = require("./commands/doneTask.js");
-const deleteTask = require("./commands/deleteTask.js");
-const reportTask = require("./commands/reportTask.js");
-const { report } = require("process");
+// const add = require("./commands/add.js");
+// const list = require("./commands/listData.js");
+// const done = require("./commands/doneTask.js");
+// const deleteTask = require("./commands/deleteTask.js");
+// const reportTask = require("./commands/reportTask.js");
 
 argv = process.argv.slice(2);
 
-if (argv(2) == false || argv[0] === "help") {
+if (!argv[0] || argv[0] === "help") {
   help();
 }
 
-if (argv[0] === "add") {
-  add(argv[1], argv[2]);
-}
+// if (argv[0] === "add") {
+//   add(argv[1], argv[2]);
+// }
 
-if (argv[0] === "ls") {
-  list();
-}
+// if (argv[0] === "ls") {
+//   list();
+// }
 
-if (argv[0] === "done") {
-  done(argv[1]);
-}
+// if (argv[0] === "done") {
+//   done(argv[1]);
+// }
 
-if (argv[0] === "del") {
-  deleteTask(argv[1]);
-}
+// if (argv[0] === "del") {
+//   deleteTask(argv[1]);
+// }
 
-if (argv[0] === "report") {
-  report();
-}
+// if (argv[0] === "report") {
+//   report();
+// }
